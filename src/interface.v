86bac117e9e5f13cc3111aca261dbbf23116c2e3
Akshaya Purohit <akshaya.purohit23@gmail.com>
2017-10-04 17:06:49 -0700

    Update interface.v

--- src/interface.v
+++ src/interface.v
@@ -4,8 +4,7 @@
 module interface(input clk,reset,enable,CPOL,CPHA,
 				 input [1:0] clk_sel,
 				 input [7:0] master_data_in,slave_data_in,
-				 output [7:0] master_data_out,slave_data_out);
-				 
+				 output [7:0] master_data_out,slave_data_out);		 
 	wire [7:0] SPDR_m,SPDR_s;
 	wire sclk,ss,mosi,miso; 				 
 	master m1(clk,reset,master_data_in,enable,miso,CPOL,CPHA,clk_sel,mosi,sclk,ss,master_data_out);
