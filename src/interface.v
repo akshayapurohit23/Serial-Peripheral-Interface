af81bc59dbf00f93ed6bb2ee772173f11c827831
Akshaya Purohit <akshaya.purohit23@gmail.com>
2017-10-04 17:23:20 -0700

    Update modules

--- src/interface.v
+++ src/interface.v
@@ -2,11 +2,11 @@
 `include "slave_module.v"
 
 module interface(input clk,reset,enable,CPOL,CPHA,
-				 input [1:0] clk_sel,
-				 input [7:0] master_data_in,slave_data_in,
-				 output [7:0] master_data_out,slave_data_out);		 
-	wire [7:0] SPDR_m,SPDR_s;
-	wire sclk,ss,mosi,miso; 				 
-	master m1(clk,reset,master_data_in,enable,miso,CPOL,CPHA,clk_sel,mosi,sclk,ss,master_data_out);
-	slave s1 (sclk,reset,ss,mosi,CPOL,CPHA,slave_data_in,miso,slave_data_out);
-endmodule				 
+                 input [1:0] clk_sel,
+                 input [7:0] master_data_in,slave_data_in,
+                 output [7:0] master_data_out,slave_data_out);         
+    wire [7:0] SPDR_m,SPDR_s;
+    wire sclk,ss,mosi,miso;                  
+    master m1(clk,reset,master_data_in,enable,miso,CPOL,CPHA,clk_sel,mosi,sclk,ss,master_data_out);
+    slave s1 (sclk,reset,ss,mosi,CPOL,CPHA,slave_data_in,miso,slave_data_out);
+endmodule                 
--- src/master_module.v
+++ src/master_module.v
@@ -1,96 +1,96 @@
 `include "clock_generator.v"
 
 module master(input clk,reset,
-	      input [7:0] master_data_in,
+              input [7:0] master_data_in,
               input enable,miso,CPOL,CPHA,
-	      input [1:0] clk_sel,
-	      output mosi,
-	      output reg sclk,ss,
-	      output reg [7:0] master_data_out);		  
-	wire clk_int;
-	reg [3:0] count,next_count;
-	reg [7:0] next_SPDR,SPDR_m;
+              input [1:0] clk_sel,
+              output mosi,
+              output reg sclk,ss,
+              output reg [7:0] master_data_out);          
+    wire clk_int;
+    reg [3:0] count,next_count;
+    reg [7:0] next_SPDR,SPDR_m;
 
-	clock_generator CG (clk,CPOL,CPHA,reset,clk_sel,clk_int);
+    clock_generator CG (clk,CPOL,CPHA,reset,clk_sel,clk_int);
 
-	always @(*)
-	begin
+    always @(*)
+    begin
        if(enable)
-		begin
-			if(~(CPOL^CPHA))
-				sclk = clk_int;
-			else
-				sclk = ~clk_int;
-		end
-		else
-			sclk = CPOL;
-	end		
+        begin
+            if(~(CPOL^CPHA))
+                sclk = clk_int;
+            else
+                sclk = ~clk_int;
+        end
+        else
+            sclk = CPOL;
+    end        
 
-	always@(posedge clk_int or posedge reset)
-	begin
-		if(reset)
-			count<=0;
-		else
-			count<=next_count;
-	end
+    always@(posedge clk_int or posedge reset)
+    begin
+        if(reset)
+            count<=0;
+        else
+            count<=next_count;
+    end
 
-	always@(*)
-	begin
-		if(enable)
-		begin
-			if(count==9)
-				next_count=0;
-			else
-				next_count=count+1;
-		end
-		else
-			next_count=0;
-	end
+    always@(*)
+    begin
+        if(enable)
+        begin
+            if(count==9)
+                next_count=0;
+            else
+                next_count=count+1;
+        end
+        else
+            next_count=0;
+    end
 
-	always@(posedge clk_int or posedge reset)
-	begin
-	    if(reset) 
-			SPDR_m<=0;
-		else      
-			SPDR_m<=next_SPDR;
-	end
+    always@(posedge clk_int or posedge reset)
+    begin
+        if(reset) 
+            SPDR_m<=0;
+        else      
+            SPDR_m<=next_SPDR;
+    end
 
-	always@(*)
-	begin
-	   	if(enable)
-		begin
-			if(count==0)
-				next_SPDR=master_data_in;
-			else
-				next_SPDR={miso,SPDR_m[7:1]};	
-		end
-		else
-			next_SPDR=master_data_in;		
-	end
+    always@(*)
+    begin
+        if(enable)
+        begin
+            if(count==0)
+                next_SPDR=master_data_in;
+            else
+                next_SPDR={miso,SPDR_m[7:1]};    
+        end
+        else
+            next_SPDR=master_data_in;        
+    end
 
-	assign mosi = SPDR_m[0];
+    assign mosi = SPDR_m[0];
 
-	always@(posedge clk_int)
-	begin	
-		if(enable)
-			ss<=1;
-		else
-			ss<=0;
-	end
+    always@(posedge clk_int)
+    begin    
+        if(enable)
+            ss<=1;
+        else
+            ss<=0;
+    end
 
-	reg [7:0] data_out;
+    reg [7:0] data_out;
 
-	always@(*)
-	begin
-		if(count==9)
-		begin
-			data_out = SPDR_m;
-		end
-		else
-			data_out = data_out;
-	end
+    always@(*)
+    begin
+        if(count==9)
+        begin
+            data_out = SPDR_m;
+        end
+        else
+            data_out = data_out;
+    end
 
-	always@(posedge clk_int)
-		master_data_out <= data_out;
+    always@(posedge clk_int)
+        master_data_out <= data_out;
 
 endmodule 
